561a0999f8d78773f8858bb9ac27a77773f8b242
Pappu Kumar <digitalpappu101@gmail.com>
2022-06-16 14:57:21 +0530

    day5 assignment

---
+++ day5/glistjson/src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Todo  from "./component/Todo";
+import "./App.css"
+
+
+export default function App() {
+    return (
+      <div className="App">
+       
+       <Todo/> 
+      </div>
+    );
+   }
